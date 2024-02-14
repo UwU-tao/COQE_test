ce176e2deced28eee76ce61ad9d551fa5dbdca9e
UwU-tao <hieungo1410@gmail.com>
2024-02-15 00:53:15 +0700

    add

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ train_data = get_data('train')
 test_data = get_data('test')
 dev_data = get_data('dev')
 
-train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-valid_loader = DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+train_loader = DataLoader(train_data, batch_size=128, shuffle=True, num_workers=2)
+valid_loader = DataLoader(valid_data, batch_size=128, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_data, batch_size=128, shuffle=False, num_workers=2)
 print('Finish loading the data....')
 
 if __name__ == '__main__':
