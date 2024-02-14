26243f24625f20a1ff61cdff1dff80edf5b309cf
UwU-tao <hieungo1410@gmail.com>
2024-02-15 02:17:11 +0700

    add

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ train_data = get_data('train')
 test_data = get_data('test')
 valid_data = get_data('dev')
 
-train_loader = DataLoader(train_data, batch_size=128, shuffle=True, num_workers=2)
-valid_loader = DataLoader(valid_data, batch_size=128, shuffle=True, num_workers=2)
-test_loader = DataLoader(test_data, batch_size=128, shuffle=False, num_workers=2)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=2)
+valid_loader = DataLoader(valid_data, batch_size=64, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=False, num_workers=2)
 print('Finish loading the data....')
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -13,5 +13,4 @@ class Simple(nn.Module):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        x = self.softmax(x)
         return x
