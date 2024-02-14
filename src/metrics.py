cffb8f848d24e18124691038bbdc2085ab44aaba
UwU-tao <hieungo1410@gmail.com>
2024-02-15 02:09:59 +0700

    add

--- src/metrics.py
+++ src/metrics.py
@@ -3,14 +3,14 @@ import numpy as np
 from torchmetrics.classification import BinaryF1Score
 from torchmetrics.classification import BinaryPrecision
 from torchmetrics.classification import BinaryRecall
-from torchmetrics.classification import Accuracy
+from torchmetrics.classification import BinaryAccuracy
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-f1 = BinaryF1Score(task='binary', average='macro', threshold=0.5, num_classes=2)
-recall = BinaryRecall(task='binary', average='macro', threshold=0.5, num_classes=2)
-precision = BinaryPrecision(task='binary', average='macro', threshold=0.5, num_classes=2)
-accuracy = Accuracy(task='binary', average='macro', threshold=0.5, num_classes=2)
+f1 = BinaryF1Score()
+recall = BinaryRecall()
+precision = BinaryPrecision()
+accuracy = BinaryAccuracy()
 f1.to(device)
 recall.to(device)
 precision.to(device)
