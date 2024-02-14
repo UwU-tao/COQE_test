361f81c35a44b67175b825e77cd17cd7d7ff8016
UwU-tao <hieungo1410@gmail.com>
2024-02-15 01:11:49 +0700

    add

--- src/dataset.py
+++ src/dataset.py
@@ -29,6 +29,6 @@ class MyDataset(Dataset):
         text = self.data_dict.iloc[idx,0]
         label = self.data_dict.iloc[idx,1]
         
-        sample = {'text': text, 'label': label}
+        sample = {'text': torch.Tensor(text), 'label': torch.Tensor(label)}
 
         return sample
