cef15a54e580cb54db6b18b022072c71d0da284e
UwU-tao <hieungo1410@gmail.com>
2024-02-15 01:46:30 +0700

    add

--- src/trainer.py
+++ src/trainer.py
@@ -52,7 +52,8 @@ def train_model(settings, train_loader, valid_loader, test_loader):
             
             optimizer.zero_grad()
             text_encoded = tokenizer(text, padding=True, return_tensors='pt').to(settings['device'])
-            outs = bert(text_encoded)
+            with torch.no_grad():
+                outs = self.bert(**text_encoded)
             predictions = model(outs.pooler_output).squeeze(1)
             loss = criterion(predictions, label)
             loss.backward()
@@ -77,7 +78,8 @@ def train_model(settings, train_loader, valid_loader, test_loader):
                 label.to(settings['device'])
                 
                 text_encoded = tokenizer(text, padding=True, return_tensors='pt').to(settings['device'])
-                outs = bert(text_encoded)
+                with torch.no_grad():
+                    outs = self.bert(**text_encoded)
                 predictions = model(outs.pooler_output).squeeze(1)
                 loss = criterion(predictions, label)
                 epoch_loss += loss.item()
