fff2f0a374eafe0aa49a38900b7e1d5d5d33a555
UwU-tao <hieungo1410@gmail.com>
2024-02-15 02:28:48 +0700

    add

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from src.dataset import MyDataset
 import torch
+import os
 
 def get_data(split='train'):
     data = MyDataset(split)
@@ -7,6 +8,8 @@ def get_data(split='train'):
 
 def save_model(model, name=''):
     name = name if len(name) > 0 else 'default_model'
+    if os.path.isdir('pretrained') == False:
+        os.mkdir('pretrained')
     torch.save(model, f'pretrained/{name}.pt')
 
 
