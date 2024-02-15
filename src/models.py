9345faec9720c453065f4a6b4e0a928c3abacd0f
UwU-tao <hieungo1410@gmail.com>
2024-02-15 11:35:07 +0700

    add

--- src/models.py
+++ src/models.py
@@ -7,9 +7,11 @@ class Simple(nn.Module):
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, output_dim)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
         
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
