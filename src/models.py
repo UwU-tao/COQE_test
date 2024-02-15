c3d4fb1f50640d0f8a13a1ed920bf0f302a66c16
UwU-tao <hieungo1410@gmail.com>
2024-02-15 17:09:21 +0700

    add

--- src/models.py
+++ src/models.py
@@ -4,17 +4,16 @@ from torch import nn
 class Simple(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(Simple, self).__init__()
-        # self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc1 = nn.Linear(input_dim, output_dim)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, output_dim)
-        # self.relu = nn.ReLU()
+        self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.2)
         self.bn = nn.BatchNorm1d(hidden_dim)
         
     def forward(self, x):
         x = self.fc1(x)
-        # x = self.relu(x)
-        # x = self.bn(x)
+        x = self.relu(x)
+        x = self.bn(x)
         x = self.dropout(x)
-        # x = self.fc2(x)
+        x = self.fc2(x)
         return x
